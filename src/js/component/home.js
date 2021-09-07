c1d5b63f387a2afcc4e0943572d718ca6f110419
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-09-07 14:40:51 +0000

    simple counter end

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,28 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+import { Card } from "./card.js";
 
 //create your first component
+let cont = 0;
+
 export function Home() {
+	let one = Math.floor((cont / 1) % 10);
+	let two = Math.floor((cont / 10) % 10);
+	let three = Math.floor((cont / 100) % 10);
+	let four = Math.floor((cont / 1000) % 10);
+
+	cont += 1;
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-wrap m-0 row justify-content-center">
+			<div className="d-flex flex-wrap m-0 row justify-content-center">
+				<Card icon="far fa-clock fa-7x"></Card>
+				<Card digit={four}></Card>
+				<Card digit={three}></Card>
+				<Card digit={two}></Card>
+				<Card digit={one}></Card>
+			</div>
 		</div>
 	);
 }
